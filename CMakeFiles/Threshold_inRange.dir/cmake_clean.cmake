b0da03805127f15b43299e05621b7b0a4d65a060
Cabobalot <casey8cockrell@gmail.com>
2019-03-04 19:21:22 -0700

    First commit

---
+++ CMakeFiles/Threshold_inRange.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Threshold_inRange.dir/Threshold_inRange.cpp.o"
+  "Threshold_inRange.pdb"
+  "Threshold_inRange"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Threshold_inRange.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
